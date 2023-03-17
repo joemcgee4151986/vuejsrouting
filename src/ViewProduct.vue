9e9651cb0d514951ed91a44d0591721225d04932
BiFoS <jon89071@gmail.com>
2023-03-17 17:04:09 +0900

    added code

--- src/ViewProduct.vue
+++ src/ViewProduct.vue
@@ -1,63 +1,36 @@
 <template>
-    <div>
-        <p><strong>ID:</strong> {{ product.id }}</p>
-        <p>
-            <strong>Price:</strong> {{ (product.price - discount) | currency }}
-            <span v-if="discount > 0">(save {{ discount | currency }})</span>
-        </p>
-        <p><strong>In stock:</strong> {{ product.inStock }}</p>
-        <p>{{ product.description }}</p>
-    </div>
+  <div>
+    <h1>{{ product.name }}</h1>
+    <p><strong>ID:</strong> {{ product.id }}</p>
+    <p><strong>Price:</strong> {{ product.price | currency }}</p>
+    <p><strong>In stock:</strong> {{ product.inStock }}</p>
+    <p>{{ product.description }}</p>
+  </div>
 </template>
 
 <script>
-    import { products } from './data/products';
-
-    export default {
-        props: {
-            productId: {
-                required: true
-            }
-        },
-        data() {
-            return {
-                products: products,
-                product: null,
-                discount: 0
-            };
-        },
-        created() {
-            this.product = this.getProduct(this.productId);
-
-            if (typeof this.$route.query.discount !== 'undefined') {
-                this.discount = this.getDiscount(this.product.price, this.$route.query.discount);
-            }
-        },
-        beforeRouteUpdate(to, from, next) {
-            this.discount = this.getDiscount(this.product.price, to.query.discount);
-            this.product = this.getProduct(to.params.productId);
-
-            next();
-        },
-        methods: {
-            getProduct(productId) {
-                let match = null;
-
-                this.products.forEach(function(product) {
-                    if (product.id == productId) {
-                        match = product;
-                    }
-                });
-
-                return match;
-            },
-            getDiscount(originalPrice, percentage) {
-                if (!percentage) {
-                    return 0;
-                }
-
-                return ((originalPrice * percentage) / 100);
-            }
+import { products } from "./data/products";
+export default {
+  data() {
+    return {
+      products: products,
+      product: null,
+    };
+  },
+  created() {
+    let productId = this.$route.params.productId;
+    this.product = this.getProduct(productId);
+  },
+  methods: {
+    getProduct(productId) {
+      let match = null;
+      this.products.forEach(function (product) {
+        if (product.id == productId) {
+          match = product;
         }
-    }
+      });
+      return match;
+    },
+  },
+};
 </script>
--- src/routes.js
+++ src/routes.js
@@ -1,28 +1,10 @@
-import ProductList from './ProductList.vue';
-import ViewProduct from './ViewProduct.vue';
-import Cart from './Cart.vue';
-import Product from './Product.vue';
-import ProductReviews from './ProductReviews.vue';
-import SpecialOffer from './SpecialOffer.vue';
-import ViewProfile from './ViewProfile.vue';
+import ProductList from "./ProductList.vue";
+import Cart from "./Cart.vue";
+import ViewProduct from "./ViewProduct.vue";
 
 export const routes = [
-    { path: '', components: {
-        default: ProductList,
-        discount: SpecialOffer
-    } },
-    { path: '/products/:productId', name: 'product', props: true, component: Product, children: [
-        { path: 'details', name: 'viewProduct', props: true, component: ViewProduct },
-        { path: 'reviews', name: 'productReviews', props: true, component: ProductReviews }
-    ] },
-    {
-        path: '/user/profile',
-        name: 'viewProfile',
-        component: ViewProfile,
-        meta: {
-            isAuthRequired: true
-        }
-    },
-    { path: '/cart', component: Cart },
-    { path: '*', component: { template: '<h1>Page Not Found!</h1>' } }
+  { path: "", component: ProductList },
+  { path: "/cart", component: Cart },
+  { path: "/products/:productId", name: "viewProduct", component: ViewProduct },
+  { path: "*", component: { template: "<h1>Page Not Found!</h1>" } },
 ];
